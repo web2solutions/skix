d27270b8459d77eb1984a9df4f4abdafc5157838
Eduardo Almeida <web2solucoes@gmail.com>
2021-04-24 03:27:29 -0300

    Update Entity.js

--- src/Entities/Entity.js
+++ src/Entities/Entity.js
@@ -25,7 +25,6 @@ export class Entity {
         if (typeof name !== 'string') {
             throw new Error('Skier asset name must be a string');
         }
-        console.info('set assetName', name);
         this._assetName = name;
     }
 
