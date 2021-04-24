30047e58c9c8d9d0fcb58b044554a22b69c62353
Eduardo Almeida <web2solucoes@gmail.com>
2021-04-24 01:31:30 -0300

    use bin assets on constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,19 @@
+
+import skier_crash_bin from '../bin/skier_crash_bin';
+import skier_left_bin from '../bin/skier_left_bin';
+import skier_left_down_bin from '../bin/skier_left_down_bin';
+import skier_down_bin from '../bin/skier_down_bin';
+import skier_right_down_bin from '../bin/skier_right_down_bin';
+import skier_right_bin from '../bin/skier_right_bin';
+
+import tree_1_bin from '../bin/tree_1_bin';
+import tree_cluster_bin from '../bin/tree_cluster_bin';
+import rock_1_bin from '../bin/rock_1_bin';
+import rock_2_bin from '../bin/rock_2_bin';
+import jump_ramp_bin from '../bin/jump_ramp_bin';
+
+
+
 export const GAME_WIDTH = window.innerWidth;
 export const GAME_HEIGHT = window.innerHeight;
 
@@ -11,21 +27,23 @@ export const TREE = 'tree';
 export const TREE_CLUSTER = 'treeCluster';
 export const ROCK1 = 'rock1';
 export const ROCK2 = 'rock2';
+export const JUMP_RAMP = 'jumpRamp';
 
 export const SKIER_STARTING_SPEED = 10;
 export const SKIER_DIAGONAL_SPEED_REDUCER = 1.4142;
 
 export const ASSETS = {
-    [SKIER_CRASH]: 'img/skier_crash.png',
-    [SKIER_LEFT]: 'img/skier_left.png',
-    [SKIER_LEFTDOWN]: 'img/skier_left_down.png',
-    [SKIER_DOWN]: 'img/skier_down.png',
-    [SKIER_RIGHTDOWN]: 'img/skier_right_down.png',
-    [SKIER_RIGHT]: 'img/skier_right.png',
-    [TREE] : 'img/tree_1.png',
-    [TREE_CLUSTER] : 'img/tree_cluster.png',
-    [ROCK1] : 'img/rock_1.png',
-    [ROCK2] : 'img/rock_2.png'
+    [SKIER_CRASH]: skier_crash_bin.img,
+    [SKIER_LEFT]: skier_left_bin.img,
+    [SKIER_LEFTDOWN]: skier_left_down_bin.img,
+    [SKIER_DOWN]: skier_down_bin.img,
+    [SKIER_RIGHTDOWN]: skier_right_down_bin.img,
+    [SKIER_RIGHT]: skier_right_bin.img,
+    [TREE] : tree_1_bin.img,
+    [TREE_CLUSTER] : tree_cluster_bin.img,
+    [ROCK1] : rock_1_bin.img,
+    [ROCK2]: rock_2_bin.img,
+    [JUMP_RAMP]: jump_ramp_bin.img
 };
 
 export const OBSTACLE_SIZE = {
