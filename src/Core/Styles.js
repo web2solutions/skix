5a122755493dbef17c582e05a36193c9b7a41294
Eduardo Almeida <web2solucoes@gmail.com>
2021-04-25 01:57:40 -0300

    Create Styles.js

---
+++ src/Core/Styles.js
@@ -0,0 +1,16 @@
+export const wrapperStyle = `
+    width: 100vw;
+    height: 100vh;
+    padding: 0px;
+    margin: 0px;
+`;
+
+export const statsBoard = `
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    border: dotted blue 1px;
+    background: white;
+    width: 20vw;
+    height: 20vh;
+`;
